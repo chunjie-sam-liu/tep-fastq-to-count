9d2af0b40051e9844ab0bd69636e8d40657916eb
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-07-18 00:00:46 +0800

    update docker

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,14 @@
 FROM ubuntu:20.04
+LABEL maintainer="Chun-Jie Liu <chunjie.sam.liu@gmail.com>"
+
+ENV SHELL /usr/bin/bash
+
+RUN apt-get update -qq \
+  && apt-get -y --no-install-recommends install ping
+
+# ENTRYPOINT ["/bin/echo", "Hello"]
+
+# CMD [ "/bin/echo", "Hello world" ]
+
+# CMD [ "ping", "localhost" ]
+CMD [ "" ]
---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2020-07-22 14:40:58
+# @DESCRIPTION:
+
+# Number of input parameters
+docker build -t chunjiesamliu/tep-pipeline:0.1 .
+
+echo "-----------------------Image built done-----------------------"
+
+docker run --rm chunjiesamliu/tep-pipeline:0.1
