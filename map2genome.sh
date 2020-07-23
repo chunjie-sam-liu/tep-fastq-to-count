95f5151e99bbe662a573178e63325ba49208eceb
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-07-23 17:20:37 +0800

    maping2genome

---
+++ map2genome.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2020-07-23 15:33:32
+# @DESCRIPTION:
+
+# Number of input parameters
+STAR --runThredN 10 \
+  --genomeDir /refdata \
+  --readFilesIn fq1 fq2 \
