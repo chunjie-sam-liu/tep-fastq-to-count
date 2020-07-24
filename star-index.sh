2c5fa24448f1ccb36c1c53092fc2775297cbcc7e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-07-24 13:43:48 +0800

    star index

--- star-index.sh
+++ star-index.sh
@@ -5,10 +5,10 @@
 # @DESCRIPTION:
 
 # Number of input parameters
-docker run -v /workspace/liucj/refdata/star-genome-index:/refdata chunjiesamliu/tep-pipeline:0.1 \
+docker run -v /workspace/liucj/refdata/star-genome-index-new:/refdata chunjiesamliu/tep-pipeline:0.1 \
   STAR --runThreadN 80 \
   --runMode genomeGenerate \
   --genomeDir /refdata \
-  --genomeFastaFiles /refdata/hg19.fa \
-  --sjdbGTFfile /refdata/gencode.v19.annotation.gtf \
+  --genomeFastaFiles /refdata/hg19-new.fa \
+  --sjdbGTFfile /refdata/Homo_sapiens.GRCh37.75-new.gtf \
   --sjdbOverhang 100
