735438b05f413837bc8fd7f2e2fd74e10befaf1d
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-07-27 10:10:19 +0800

    test mapping

--- map2genome.sh
+++ map2genome.sh
@@ -32,3 +32,30 @@ docker run -v /workspace/liucj/refdata/star-genome-index-new:/refdata \
   --limitBAMsortRAM 20000000000 \
   --outFileNamePrefix map_ERR1769022_
 
+
+
+docker run -v /workspace/liucj/refdata/star-genome-index-new:/refdata \
+  -v /home/liucj/tmp/tep-cancer-cell-2017/fastq-files:/home/vault \
+  chunjiesamliu/tep-pipeline:0.1 \
+  STAR --genomeDir /refdata \
+  --readFilesIn SRR5019883_1.fastq.gz \
+  --readFilesCommand gunzip -c \
+  --runThreadN 24 \
+  --genomeLoad NoSharedMemory \
+  --outFilterMultimapNmax 20 \
+  --alignSJoverhangMin 8 \
+  --alignSJDBoverhangMin 1 \
+  --outFilterMismatchNmax 999 \
+  --outFilterMismatchNoverReadLmax 0.04 \
+  --alignIntronMin 20 \
+  --alignIntronMax 1000000 \
+  --alignMatesGapMax 1000000 \
+  --outSAMheaderHD @HD VN:1.4 SO:coordinate \
+  --outSAMunmapped Within \
+  --outFilterType BySJout \
+  --outSAMattributes NH HI AS NM MD \
+  --outSAMtype BAM SortedByCoordinate \
+  --quantMode GeneCounts \
+  --sjdbScore 1 \
+  --limitBAMsortRAM 20000000000 \
+  --outFileNamePrefix SRR5019883_1.fastq.gz
