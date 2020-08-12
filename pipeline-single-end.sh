18f3adfffc2bf12d4eb9066ac2c889e701cd4eff
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-08-12 13:27:15 +0800

    single end

---
+++ pipeline-single-end.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2020-08-12 14:55:15
+# @DESCRIPTION:
+
+# Number of input parameters
+parse_params ()
+{
+    local existing_named
+    local ARGV=() # un-named params
+    local ARGN=() # named params
+    local ARGO=() # options (--params)
+    echo "local ARGV=(); local ARGN=(); local ARGO=();"
+    while [[ "$1" != "" ]]; do
+        # Escape asterisk to prevent bash asterisk expansion, and quotes to prevent string breakage
+        _escaped=${1/\*/\'\"*\"\'}
+        _escaped=${_escaped//\'/\\\'}
+        _escaped=${_escaped//\"/\\\"}
+        # If equals delimited named parameter
+        nonspace="[^[:space:]]"
+        if [[ "$1" =~ ^${nonspace}${nonspace}*=..* ]]; then
+            # Add to named parameters array
+            echo "ARGN+=('$_escaped');"
+            # key is part before first =
+            local _key=$(echo "$1" | cut -d = -f 1)
+            # Just add as non-named when key is empty or contains space
+            if [[ "$_key" == "" || "$_key" =~ " " ]]; then
+                echo "ARGV+=('$_escaped');"
+                shift
+                continue
+            fi
+            # val is everything after key and = (protect from param==value error)
+            local _val="${1/$_key=}"
+            # remove dashes from key name
+            _key=${_key//\-}
+            # skip when key is empty
+            # search for existing parameter name
+            if (echo "$existing_named" | grep "\b$_key\b" >/dev/null); then
+                # if name already exists then it's a multi-value named parameter
+                # re-declare it as an array if needed
+                if ! (declare -p _key 2> /dev/null | grep -q 'declare \-a'); then
+                    echo "$_key=(\"\$$_key\");"
+                fi
+                # append new value
+                echo "$_key+=('$_val');"
+            else
+                # single-value named parameter
+                echo "local $_key='$_val';"
+                existing_named=" $_key"
+            fi
+        # If standalone named parameter
+        elif [[ "$1" =~ ^\-${nonspace}+ ]]; then
+            # remove dashes
+            local _key=${1//\-}
+            # Just add as non-named when key is empty or contains space
+            if [[ "$_key" == "" || "$_key" =~ " " ]]; then
+                echo "ARGV+=('$_escaped');"
+                shift
+                continue
+            fi
+            # Add to options array
+            echo "ARGO+=('$_escaped');"
+            echo "local $_key=\"$_key\";"
+        # non-named parameter
+        else
+            # Escape asterisk to prevent bash asterisk expansion
+            _escaped=${1/\*/\'\"*\"\'}
+            echo "ARGV+=('$_escaped');"
+        fi
+        shift
+    done
+}
+
+#--------------------------- DEMO OF THE USAGE -------------------------------
+
+show_use ()
+{
+    eval $(parse_params "$@")
+    # --
+    echo "${ARGV[0]}" # print first unnamed param
+    echo "${ARGV[1]}" # print second unnamed param
+    echo "${ARGN[0]}" # print first named param
+    echo "${ARG0[0]}" # print first option param (--force)
+    echo "$anyparam"  # print --anyparam value
+    echo "$k"         # print k=5 value
+    echo "${multivalue[0]}" # print first value of multi-value
+    echo "${multivalue[1]}" # print second value of multi-value
+    [[ "$force" == "force" ]] && echo "\$force is set so let the force be with you"
+}
+
+show_use "param 1" --anyparam="my value" param2 k=5 --force --multi-value=test1 --multi-value=test2
